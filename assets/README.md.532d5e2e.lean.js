18476bea521a391f576ecaf1a778e0a4b2019b0a
davestewart <info@davestewart.co.uk>
2023-10-13 18:08:45 +0000

    Deploying to gh-pages from @ spectre-org/spectre-docs@c7f9e74386bbb3ab91ec3209d8adfb24d50b0540 🚀

--- assets/README.md.532d5e2e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as a,a as n}from"./chunks/framework.05eef085.js";const m=JSON.parse('{"title":"Spectre Docs","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),o={name:"README.md"},t=n("",9),l=[t];function p(r,c,i,d,h,u){return s(),a("div",null,l)}const v=e(o,[["render",p]]);export{m as __pageData,v as default};
---
+++ assets/README.md.a43c932e.js
@@ -0,0 +1,12 @@
+import{_ as e,o as s,c as a,a as n}from"./chunks/framework.8669db73.js";const m=JSON.parse('{"title":"Spectre Docs","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),o={name:"README.md"},t=n(`<h1 id="spectre-docs" tabindex="-1">Spectre Docs <a class="header-anchor" href="#spectre-docs" aria-label="Permalink to &quot;Spectre Docs&quot;">​</a></h1><blockquote><p>Spectre CSS docs, rewritten in Markdown and powered by VitePress</p></blockquote><h2 id="overview" tabindex="-1">Overview <a class="header-anchor" href="#overview" aria-label="Permalink to &quot;Overview&quot;">​</a></h2><p>This repo is likely a temporary repository whilst the old Pug-based docs are migrated to Markdown and VuePress.</p><h2 id="development" tabindex="-1">Development <a class="header-anchor" href="#development" aria-label="Permalink to &quot;Development&quot;">​</a></h2><p>Migration:</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-light"><code><span class="line"><span style="color:#24292e;"># Preview the old docs</span></span>
+<span class="line"><span style="color:#24292e;">npm run old:preview</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;"># Migrate watch the old docs to markdown</span></span>
+<span class="line"><span style="color:#24292e;">npm run old:convert</span></span></code></pre></div><p>Testing:</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-light"><code><span class="line"><span style="color:#24292e;"># Run VuePress</span></span>
+<span class="line"><span style="color:#24292e;">npm run docs:dev</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;"># Generate the docs</span></span>
+<span class="line"><span style="color:#24292e;">npm run docs:build</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;"># Preview the built docs in the browser</span></span>
+<span class="line"><span style="color:#24292e;">npm run docs:dev</span></span></code></pre></div>`,9),l=[t];function p(r,c,i,d,h,u){return s(),a("div",null,l)}const v=e(o,[["render",p]]);export{m as __pageData,v as default};
