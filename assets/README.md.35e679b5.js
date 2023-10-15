874a2890091e6d5c5ee2aed2fd55aeecdb61bae0
davestewart <info@davestewart.co.uk>
2023-10-15 09:26:34 +0000

    Deploying to gh-pages from @ spectre-org/spectre-docs@831d5fdf916d4962e42b03dae60fdb14b26f7379 🚀

--- assets/README.md.35e679b5.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,o as s,c as a,a as n}from"./chunks/framework.8669db73.js";const m=JSON.parse('{"title":"Spectre Docs","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),t={name:"README.md"},o=n(`<h1 id="spectre-docs" tabindex="-1">Spectre Docs <a class="header-anchor" href="#spectre-docs" aria-label="Permalink to &quot;Spectre Docs&quot;">​</a></h1><blockquote><p>Spectre CSS docs, rewritten in Markdown and powered by VitePress</p></blockquote><h2 id="development" tabindex="-1">Development <a class="header-anchor" href="#development" aria-label="Permalink to &quot;Development&quot;">​</a></h2><p>Scripts:</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-light"><code><span class="line"><span style="color:#24292e;"># Run VitePress</span></span>
-<span class="line"><span style="color:#24292e;">npm run dev</span></span>
-<span class="line"><span style="color:#24292e;"></span></span>
-<span class="line"><span style="color:#24292e;"># Generate the docs</span></span>
-<span class="line"><span style="color:#24292e;">npm run build</span></span>
-<span class="line"><span style="color:#24292e;"></span></span>
-<span class="line"><span style="color:#24292e;"># Preview the built docs in the browser</span></span>
-<span class="line"><span style="color:#24292e;">npm run dev</span></span></code></pre></div>`,5),p=[o];function l(c,r,i,d,_,h){return s(),a("div",null,p)}const b=e(t,[["render",l]]);export{m as __pageData,b as default};
