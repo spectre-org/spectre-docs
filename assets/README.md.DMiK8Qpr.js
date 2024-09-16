15f403976ae648b3f2476b81697a8ebcf8e92814
davestewart <info@davestewart.co.uk>
2024-09-16 18:42:29 +0000

    Deploying to gh-pages from @ spectre-org/spectre-docs@a0d5712a1837ebaa6b04915e665b3e080374fafc 🚀

---
+++ assets/README.md.DMiK8Qpr.js
@@ -0,0 +1,8 @@
+import{_ as e,c as a,a3 as n,o as p}from"./chunks/framework.DzIAwTRr.js";const m=JSON.parse('{"title":"Spectre Docs","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),o={name:"README.md"};function t(l,s,c,r,i,d){return p(),a("div",null,s[0]||(s[0]=[n(`<h1 id="spectre-docs" tabindex="-1">Spectre Docs <a class="header-anchor" href="#spectre-docs" aria-label="Permalink to &quot;Spectre Docs&quot;">​</a></h1> <blockquote><p>Spectre CSS docs, rewritten in Markdown and powered by VitePress</p></blockquote> <h2 id="development" tabindex="-1">Development <a class="header-anchor" href="#development" aria-label="Permalink to &quot;Development&quot;">​</a></h2> <p>Scripts:</p> <div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki github-light vp-code" tabindex="0"><code><span class="line"><span style="color:#6A737D;"># build and preview docs for development</span></span>
+<span class="line"><span style="color:#6F42C1;">npm</span><span style="color:#032F62;"> run</span><span style="color:#032F62;"> dev</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#6A737D;"># generate static docs</span></span>
+<span class="line"><span style="color:#6F42C1;">npm</span><span style="color:#032F62;"> run</span><span style="color:#032F62;"> build</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#6A737D;"># preview static docs</span></span>
+<span class="line"><span style="color:#6F42C1;">npm</span><span style="color:#032F62;"> run</span><span style="color:#032F62;"> preview</span></span></code></pre></div>`,9)]))}const h=e(o,[["render",t]]);export{m as __pageData,h as default};
